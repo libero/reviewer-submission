d4338b377b16f0d079abda7a88a1a5c6437ecad8
Peter East <petereast+gpg@pm.me>
2019-07-24 10:19:24 +0100

    Add healthcheck and basic test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,4 +10,9 @@ export class AppController {
   async serveRoot(@Res() res): Promise<any> {
     res.sendFile(resolve('../client/dist/index.html'));
   }
+
+  @Get('/health')
+  async sendHealthCheck(@Res() res): Promise<any> {
+    return res.status(200).json({ok: true});
+  }
 }
