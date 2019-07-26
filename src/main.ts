81c7f77fdc1e912ffe2a42fec72b8a5e5ba10dd3
Peter East <petereast+gpg@pm.me>
2019-07-26 11:59:19 +0100

    Move config module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { ConfigService } from './packages-old/config/config.service';
+import { ConfigService } from './modules/config/config.service';
 import { join } from 'path';
 
 async function bootstrap() {
--- src/packages-old/auth/graphql.guard.ts
+++ src/packages-old/auth/graphql.guard.ts
@@ -3,6 +3,7 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
+
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
