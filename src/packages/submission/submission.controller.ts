e0d31bd35dbb75ec762803bed1ded24501ac9195
Peter East <petereast+gpg@pm.me>
2019-07-30 13:54:22 +0100

    [wip] Start working on Submission Repo

--- src/packages/submission/submission.controller.ts
+++ src/packages/submission/submission.controller.ts
@@ -1,6 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import { Submission } from './submission.entity';
-import { SubmissionRepository } from './submission.repository';
+import { ISubmission, SubmissionRepository } from './submission.repository';
 import { Option, None, Some } from 'funfix';
 
 export class SubmissionController {
@@ -12,34 +12,36 @@ export class SubmissionController {
 
   async findAll(): Promise<Submission[]> {
     return await this.repository
-      .map(repo => repo.findAll())
-      .getOrElseL(() => {
-
-        // tslint:disable-next-line
-        console.error('No repo');
-
-        return [];
-      });
+      .map(async repo =>
+        (await repo.findAll()).map((isub: ISubmission) => new Submission(isub)),
+      )
+      .get();
   }
 
   async start(): Promise<Submission> {
     const submission: Submission = Submission.make(uuid());
 
-    return this.repository.map(repo => repo.save(submission)).get();
+    return await this.repository
+      .map(async repo => new Submission(await repo.save(submission)))
+      .get();
   }
 
   async findOne(id: string): Promise<Submission> {
-    return await this.repository.map(async repo => (await repo.findById(id)).get()).get();
+    return await this.repository
+      .map(async repo => new Submission((await repo.findById(id)).get()))
+      .get();
   }
 
   async changeTitle(id: string, title: string): Promise<Submission> {
     return await this.repository
       .map(async repo => {
-        const submission: Submission = (await repo.findById(id)).get();
+        const submission: Submission = new Submission(
+          (await repo.findById(id)).get(), // XXX: Will error if submission can't be found
+        );
 
         submission.changeTitle(title);
 
-        return await repo.save(submission);
+        return new Submission(await repo.save(submission));
       })
       .get();
   }
--- src/packages/submission/submission.repository.ts
+++ src/packages/submission/submission.repository.ts
@@ -2,10 +2,12 @@ import { Submission } from './submission.entity';
 import { Option } from 'funfix';
 
 export interface SubmissionRepository {
-  findAll(): Promise<Submission[]>;
-  findById(id: string): Promise<Option<Submission>>;
-  save(subm: Submission): Promise<Submission>;
+  findAll(): Promise<ISubmission[]>;
+  findById(id: string): Promise<Option<ISubmission>>;
+  save(subm: ISubmission): Promise<ISubmission>;
 }
+
+// I'm treating ISubmission as a DTO for submission
 
 export interface ISubmission {
   id: string;
