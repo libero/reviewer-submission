a35a7798e4897acda5cc20e602dd3a371135e552
Peter Hooper <diversemix@gmail.com>
2019-08-07 08:39:20 +0100

    fix lint errors

--- src/packages/core/index.ts
+++ src/packages/core/index.ts
@@ -1,39 +1,40 @@
-import {string, refinement, TypeOf, Type } from 'io-ts';
+import {string, refinement, TypeOf } from 'io-ts';
 
 const uuidCheck = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 const UUIDRefinement = refinement(string, (str: string) => uuidCheck.test(str), 'Uuid');
 
+// tslint:disable:max-classes-per-file
 export type Uuid = TypeOf<typeof UUIDRefinement>;
 
 // All this is testing trying to make the UUID types more specific
 // https://github.com/Microsoft/TypeScript/issues/202#issuecomment-498201335
 export function branded<T, Brand>() {
-  return class Type {
-      value: Type;
+  return class BrandedType {
+      value: BrandedType;
       '__ kind': Brand;
-      static toBranded<Cls extends typeof Type>(this: Cls, t: T) {
+      static toBranded<Cls extends typeof BrandedType>(this: Cls, t: T) {
         return t as unknown as InstanceType<Cls>;
       }
-      static fromBranded<Cls extends typeof Type>(this:Cls, b: InstanceType<Cls>) {
-        return b as unknown as T
+      static fromBranded<Cls extends typeof BrandedType>(this: Cls, b: InstanceType<Cls>) {
+        return b as unknown as T;
       }
-      static Type: Type;
-  }
+      static Type: BrandedType;
+  };
 }
 
 // Modified for UUID
 export function uuidType<Brand>() {
-  return class Type {
-      value: Type;
+  return class UuidType {
+      value: UuidType;
       '__ kind': Brand;
-      static fromUuid<Cls extends typeof Type>(this: Cls, t: Uuid) {
+      static fromUuid<Cls extends typeof UuidType>(this: Cls, t: Uuid) {
         return t as unknown as InstanceType<Cls>;
       }
-      static toUuid<Cls extends typeof Type>(this:Cls, b: InstanceType<Cls>) {
-        return b as unknown as Uuid
+      static toUuid<Cls extends typeof UuidType>(this: Cls, b: InstanceType<Cls>) {
+        return b as unknown as Uuid;
       }
-      static Type: Type;
-  }
+      static Type: UuidType;
+  };
 }
 
 /* THIS CODE WILL ERROR:
@@ -45,4 +46,3 @@ var y : VegtableId = VegtableId.toUuid('928a6b06-1120-4970-a0dc-3c48102d46ad');
 
 x = y;
 */
-
