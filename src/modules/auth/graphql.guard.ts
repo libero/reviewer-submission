f95d0a78e5e6a878f3e44e3a742aa3242c175293
Peter East <petereast@users.noreply.github.com>
2019-09-09 10:58:59 +0100

    Add a strawman token interface (#201)
    * Add a strawman token interface
    
    * Generate a valid token in continuum-auth
    
    * lint
    
    * Add decouple from elife
    
    * Fix type error

--- src/modules/auth/graphql.guard.ts
+++ src/modules/auth/graphql.guard.ts
@@ -3,7 +3,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-
 export class GqlAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
--- src/modules/auth/types.ts
+++ src/modules/auth/types.ts
@@ -4,3 +4,24 @@ export interface JwtPayload {
   iat: number;
   iss: string;
 }
+
+// TODO: Move this into somewhere else - no copy & paste!
+export interface UserIdentity { // don't forget this is merged with the rest of the JWT standard fields
+  token_id: string; // Generated, unique per token
+  token_version: '0.1-alpha'; // Generated, hardcoded
+  identity: { // Unique per user, lookup from profiles, people, user management services
+    user_id: string;
+    external: Array<{
+      id: string;
+      domain: string; // e.g. "elife-profiles", "elife-people", "orcid"
+    }>;
+  };
+
+  // Generic user roles, basically to know which
+  // So a user is associated with a `journal` when they've got a role with it?
+  roles: Array<{
+    journal: string; // e.g. "elife"
+    kind: string; // e.g. "author", "reviewer", e.t.c
+  }>;
+  meta: unknown; // could be anything
+}
