08a2528c1ebd9a46593bf9d3c60f5f1a13deef7c
Cory <c.donkin@elifesciences.org>
2019-11-01 16:25:18 +0000

    remove reference to client (#442)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,11 +7,6 @@ import { resolve } from 'path';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/')
-  async serveRoot(@Res() res): Promise<void> {
-    res.sendFile(resolve('../client/dist/index.html'));
-  }
-
   @Get('/health')
   async sendHealthCheck(@Res() res): Promise<{ok: true}> {
     return res.status(200).json({ok: true});
