0f19a502021cfba0481597d7135016d0970e89c7
Erez Odier <erez.odier@gmail.com>
2020-02-03 17:59:18 +0000

    apollo server

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import * as pino from 'pino';
 
 const Logger = pino();
 
-const ServiceLogger = Logger.child({ service: 'continuum-adaptor' });
+const ServiceLogger = Logger.child({ service: 'reviewer-submission' });
 
 export const InfraLogger = ServiceLogger.child({ realm: 'infra' });
 export const DomainLogger = ServiceLogger.child({ realm: 'domain' });
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import * as express from 'express';
 import { Express, Request, Response } from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 import config from './config';
-
 import errorHandler from './middleware/error-handler';
 import { InfraLogger as logger } from './logger';
+import { join } from 'path';
+import { importSchema } from 'graphql-import';
 
 const init = async (): Promise<void> => {
     logger.info('Starting service');
     // Start the application
     const app: Express = express();
+
+    try {
+        const typeDefs = await importSchema(join(__dirname + '/modules/submission-adaptor/submission.graphql'));
+        const apolloServer = new ApolloServer({ typeDefs });
+
+        apolloServer.applyMiddleware({ app });
+    } catch (e) {
+        logger.trace(e);
+    }
 
     // This is how we do dependency injection at the moment
     app.get('/health', (_: Request, res: Response) => res.sendStatus(200));
