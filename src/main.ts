18d2dde43b7a970ad3a5490d91524520d1843ed9
norisalsaadie <noris@alsaadie.com>
2020-02-04 09:45:25 +0000

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as helmet from 'helmet';
 import { Express, Request, Response } from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import config from './config';
@@ -11,11 +12,11 @@ const init = async (): Promise<void> => {
     logger.info('Starting service');
     // Start the application
     const app: Express = express();
-
+    // best to mount helmet so soon as possible to ensure headers are set: defaults - https://www.npmjs.com/package/helmet#how-it-works
+    app.use(helmet());
     try {
         const typeDefs = await importSchema(join(__dirname + '/modules/submission-adaptor/submission.graphql'));
         const apolloServer = new ApolloServer({ typeDefs });
-
         apolloServer.applyMiddleware({ app });
     } catch (e) {
         logger.trace(e);
