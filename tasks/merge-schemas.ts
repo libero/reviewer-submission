9510cb1a41916174086a9baab9fecaf0bcfab270
norisalsaadie <noris@alsaadie.com>
2020-02-10 11:59:28 +0000

    Add some comments

--- tasks/merge-schemas.ts
+++ tasks/merge-schemas.ts
@@ -8,6 +8,10 @@ const importOptions = {
     skipGraphQLImport: true,
 };
 
+// 1. ensure lint-schemas dir exists
+// 2. import and merge schemas in the same way as the server
+// 3. write the file to a location that is gitignored.
+// 4. lint -> exit 1 for fail, 0 for pass.
 util.promisify(fs.mkdir)('lint-schemas', { recursive: true })
     .then(_ => importSchema(path.join(__dirname, '..', 'src', './schemas/**/*.graphql'), importOptions))
     .then(merged => util.promisify(fs.writeFile)('lint-schemas/all.graphql', merged, { encoding: 'utf8' }))
