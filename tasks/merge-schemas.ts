132130f36b8ed9ee6bc01da96c1d68d5d468050c
norisalsaadie <noris@alsaadie.com>
2020-02-10 11:24:36 +0000

    Use same merging as main.tsc

--- tasks/merge-schemas.ts
+++ tasks/merge-schemas.ts
@@ -1,12 +1,15 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as util from 'util';
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
-
-const typesArray = fileLoader(path.join(__dirname, '..', 'src', './schemas'));
-const merged = mergeTypes(typesArray, { all: true });
+import { importSchema } from 'graphql-import';
 
 util.promisify(fs.mkdir)('lint-schemas', { recursive: true })
-    .then(_ => util.promisify(fs.writeFile)('lint-schemas/all.graphql', merged, { encoding: 'utf8' }))
+    .then(_ =>
+        importSchema(path.join(__dirname, '..', 'src', './schemas/**/*.graphql'), {
+            forceGraphQLImport: false,
+            skipGraphQLImport: true,
+        }),
+    )
+    .then(merged => util.promisify(fs.writeFile)('lint-schemas/all.graphql', merged, { encoding: 'utf8' }))
     .then(_ => console.log('done'))
     .catch((e: Error) => console.log('Error', e));
