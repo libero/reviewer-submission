4f89cbe574008f85be55beefbbde3ebd5d294681
diversemix <diversemix@gmail.com>
2020-02-11 15:03:13 +0000

    get submission tests working

--- src/repositories/knex-mock.ts
+++ src/repositories/knex-mock.ts
@@ -1,11 +1,26 @@
-
 export class MockKnex {
-    insert(): MockKnex { return this; }
-    withSchema(): MockKnex { return this; }
-    into(): MockKnex { return this; }
-    select(): MockKnex { return this; }
-    from(): MockKnex { return this; }
-    where(): MockKnex { return this; }
-    returning(): MockKnex { return this; }
-    delete(): boolean { return true; }
-};
+    insert(): MockKnex {
+        return this;
+    }
+    withSchema(): MockKnex {
+        return this;
+    }
+    into(): MockKnex {
+        return this;
+    }
+    select(): MockKnex {
+        return this;
+    }
+    from(): MockKnex {
+        return this;
+    }
+    where(): MockKnex {
+        return this;
+    }
+    returning(): MockKnex {
+        return this;
+    }
+    delete(): boolean {
+        return true;
+    }
+}
--- src/repositories/knex-submission.test.ts
+++ src/repositories/knex-submission.test.ts
@@ -3,13 +3,33 @@ import * as Knex from 'knex';
 import { KnexSubmissionRepository } from './knex-submission';
 import { SubmissionId } from '../types/submission';
 import { MockKnex } from './knex-mock';
+import { SubmissionMapper } from '../entities/submission';
 
+const testSubmission = {
+    id: SubmissionId.fromUuid(uuid()),
+    title: 'The title',
+    status: 'INITIAL',
+    createdBy: '123',
+    updated: new Date(),
+    articleType: 'newspaper',
+};
+
+const testSubmission2 = {
+    id: SubmissionId.fromUuid(uuid()),
+    title: 'Another title',
+    status: 'INITIAL',
+    createdBy: '124',
+    updated: new Date(),
+    articleType: 'journal',
+};
+
+const testSubmissionItems = [SubmissionMapper.toDto(testSubmission), SubmissionMapper.toDto(testSubmission2)];
 
 describe('Knex Submission Repository', () => {
     let mockKnex: MockKnex;
 
     beforeEach(() => {
-        jest.resetAllMocks()
+        jest.resetAllMocks();
         mockKnex = new MockKnex();
         mockKnex.insert = jest.fn().mockReturnValue(mockKnex);
         mockKnex.withSchema = jest.fn().mockReturnValue(mockKnex);
@@ -28,30 +48,47 @@ describe('Knex Submission Repository', () => {
     });
 
     it('Can findById', () => {
-        const id = SubmissionId.fromUuid(uuid());
-        mockKnex.where = jest.fn().mockReturnValue(mockKnex);
+        mockKnex.where = jest.fn().mockReturnValue(testSubmissionItems);
         const repo = new KnexSubmissionRepository((mockKnex as unknown) as Knex);
-        expect(repo.findById(id)).resolves.toEqual(1);
-    });
-/*
-    it('Can findAll', () => {
-        const repo = new KnexSubmissionRepository((mockKnex as unknown) as Knex);
-        expect(repo.findAll()).resolves.toEqual(true);
-    });
-*/
-    it('Can save', () => {
-        const id = SubmissionId.fromUuid(uuid());
-        const submission = {
-            id: id,
-            title: '',
+        return expect(repo.findById(testSubmission.id)).resolves.toMatchObject({
+            id: testSubmission.id,
+            title: 'The title',
             status: 'INITIAL',
             createdBy: '123',
-            updated: new Date(),
-            articleType: '',
-        };
+            articleType: 'newspaper',
+        });
+    });
 
+    it('Can findAll', async () => {
+        mockKnex.from = jest.fn().mockReturnValue(testSubmissionItems);
         const repo = new KnexSubmissionRepository((mockKnex as unknown) as Knex);
-        expect(repo.save(submission)).resolves.toEqual(submission);
+        const result = await repo.findAll();
+        expect(result).toHaveLength(2);
+        expect(result[0]).toMatchObject({
+            id: testSubmission.id,
+            title: 'The title',
+            status: 'INITIAL',
+            createdBy: '123',
+            articleType: 'newspaper',
+        });
+        expect(result[1]).toMatchObject({
+            id: testSubmission2.id,
+            title: 'Another title',
+            status: 'INITIAL',
+            createdBy: '124',
+            articleType: 'journal',
+        });
+    });
+
+    it('Can save', async () => {
+        const repo = new KnexSubmissionRepository((mockKnex as unknown) as Knex);
+        await expect(repo.save(testSubmission)).resolves.toMatchObject({
+            id: testSubmission.id,
+            title: 'The title',
+            status: 'INITIAL',
+            createdBy: '123',
+            articleType: 'newspaper',
+        });
         expect(mockKnex.withSchema).toBeCalledWith('public');
         expect(mockKnex.into).toBeCalledWith('manuscript');
         expect(mockKnex.returning).toBeCalledWith('id');
