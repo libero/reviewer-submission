8aea728ff31e6ea99ba74eac3c882874b7ed6136
Cory <c.donkin@elifesciences.org>
2020-02-19 14:46:20 +0000

    add extra test to submission model

--- src/submission/models/submission.test.ts
+++ src/submission/models/submission.test.ts
@@ -3,20 +3,39 @@ import Submission from './submission';
 import { SubmissionId } from '../submission';
 
 describe('Submission Entity', () => {
-    it('creates a new entity properly', () => {
-        const id = SubmissionId.fromUuid(uuid());
-        const submission = new Submission({
-            id: id,
-            title: '',
-            status: 'INITIAL',
-            createdBy: '123',
-            updated: new Date(),
-            articleType: 'researchArticle',
+    describe('constructor', () => {
+        it('creates a new entity and correctly sets properties from constructor params', () => {
+            const id = SubmissionId.fromUuid(uuid());
+            const submission = new Submission({
+                id: id,
+                title: '',
+                status: 'INITIAL',
+                createdBy: '123',
+                updated: new Date(),
+                articleType: 'researchArticle',
+            });
+
+            expect(submission).toBeInstanceOf(Submission);
+            expect(submission.title).toBe('');
+            expect(submission.id).toBe(id);
+            expect(submission.status).toBe('INITIAL');
+            expect(submission.articleType).toBe('researchArticle');
+            expect(submission.createdBy).toBe('123');
+            expect(submission.updated).toBeDefined();
         });
 
-        expect(submission).toBeInstanceOf(Submission);
-        expect(submission.title).toBeDefined();
-        expect(submission.id).toBe(id);
-        expect(submission.updated).toBeDefined();
+        it('throws if an invalid articleType is passed', () => {
+            const id = SubmissionId.fromUuid(uuid());
+            expect(() => {
+                new Submission({
+                    id: id,
+                    title: '',
+                    status: 'INITIAL',
+                    createdBy: '123',
+                    updated: new Date(),
+                    articleType: 'foo',
+                });
+            }).toThrow('Invalid article type');
+        });
     });
 });
