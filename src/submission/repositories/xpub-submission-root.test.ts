e75568dc449cde575b6376852f426df9ad150a60
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-02-20 18:02:05 +0000

    Pushing for pete

--- src/submission/repositories/xpub-submission-root.test.ts
+++ src/submission/repositories/xpub-submission-root.test.ts
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { v4 as uuid } from 'uuid';
-
 import XpubSubmissionRootRepository from './xpub-submission-root';
 import { SubmissionId } from '../types';
 import { SubmissionDTO } from './types';
-import { MockKnex, createMockAdapter, MockConnector } from '../../test-mocks/knex-mock';
-import { KnexTableAdapter } from 'src/knex-table-adapter';
+import { createMockAdapter } from '../../test-mocks/knex-mock';
+import { KnexTableAdapter } from '../../knex-table-adapter';
 
 const entryId = SubmissionId.fromUuid(uuid());
 const entryId2 = SubmissionId.fromUuid(uuid());
@@ -35,26 +34,22 @@ const testDatabaseEntry2 = {
 const databaseEntries = [testDatabaseEntry, testDatabaseEntry2];
 
 describe('Knex Submission Repository', () => {
-    let mockKnex: MockConnector;
     let adapter: KnexTableAdapter;
 
     beforeEach(() => {
         jest.resetAllMocks();
-
-        mockKnex = {
-            getQueryBuilder: () => {},
-        };
-
-        adapter = createMockAdapter(mockKnex);
+        adapter = createMockAdapter();
     });
 
     describe('findAll', () => {
-        it.only('returns the correct number of entries', async (): Promise<void> => {
+        it('returns the correct number of entries', async (): Promise<void> => {
+            adapter.executor = jest.fn().mockReturnValue(databaseEntries);
             const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.findAll();
             expect(result).toHaveLength(2);
         });
         it('returns a converted DTO object from the database entries', async (): Promise<void> => {
+            adapter.executor = jest.fn().mockReturnValue(databaseEntries);
             const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.findAll();
             expect(result[0]).toStrictEqual({
@@ -74,14 +69,10 @@ describe('Knex Submission Repository', () => {
                 updated: new Date('2020-02-18T15:14:53.155Z'),
             });
         });
-        it('calls the knex instance methods with the correct parameters', async (): Promise<void> => {
-            const repo = new XpubSubmissionRootRepository(adapter);
-            await repo.findAll();
-            // TESTS
-        });
     });
     describe('findById', () => {
         it('returns a converted DTO object from the database entry', () => {
+            adapter.executor = jest.fn().mockReturnValue([databaseEntries[0]]);
             const repo = new XpubSubmissionRootRepository(adapter);
             return expect(repo.findById(entryId)).resolves.toStrictEqual({
                 id: entryId,
@@ -93,38 +84,23 @@ describe('Knex Submission Repository', () => {
             });
         });
         it('returns the first entry if multiple entries are found by query', async (): Promise<void> => {
+            adapter.executor = jest.fn().mockReturnValue(databaseEntries);
             const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.findById(entryId);
             expect((result as SubmissionDTO).id).toEqual(databaseEntries[0].id);
         });
         it('returns null on no submission found', async (): Promise<void> => {
+            adapter.executor = jest.fn().mockReturnValue([]);
             const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.findById(entryId);
             expect(result).toBeNull();
         });
-        it('calls the knex instance methods with the correct parameters', async (): Promise<void> => {
-            const repo = new XpubSubmissionRootRepository(adapter);
-            await repo.findById(entryId);
-            // TESTS
-        });
     });
 
-    /*
     describe('update', () => {
-        it('calls update on knex if the entry exists', async (): Promise<void> => {
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
-            await repo.update({
-                id: entryId,
-                title: 'The title',
-                status: 'INITIAL',
-                createdBy: '123',
-                articleType: 'newspaper',
-            });
-        });
         it('updates the updated time of the entry', async (): Promise<void> => {
             const lastUpdated = databaseEntries[0].updated;
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
-            repo.findById = jest.fn().mockReturnValue({
+            adapter.executor = jest.fn().mockReturnValue({
                 id: entryId,
                 title: 'The title',
                 status: 'INITIAL',
@@ -132,6 +108,7 @@ describe('Knex Submission Repository', () => {
                 articleType: 'newspaper',
                 updated: lastUpdated,
             });
+            const repo = new XpubSubmissionRootRepository(adapter);
             const { updated } = await repo.update({
                 id: entryId,
                 title: 'The title',
@@ -143,8 +120,7 @@ describe('Knex Submission Repository', () => {
         });
         it('returns a complete DTO when passed a partial to update', async (): Promise<void> => {
             const lastUpdated = databaseEntries[0].updated;
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
-            repo.findById = jest.fn().mockReturnValue({
+            adapter.executor = jest.fn().mockReturnValue({
                 id: entryId,
                 title: 'The title',
                 status: 'INITIAL',
@@ -152,6 +128,7 @@ describe('Knex Submission Repository', () => {
                 articleType: 'newspaper',
                 updated: lastUpdated,
             });
+            const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.update({
                 id: entryId,
                 title: 'A Different Title',
@@ -166,22 +143,9 @@ describe('Knex Submission Repository', () => {
         });
     });
     describe('create', () => {
-        it('calls insert on knex', async (): Promise<void> => {
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
-            repo.findById = jest.fn().mockReturnValue(null);
-            expect(mockKnex.insert).toBeCalledTimes(0);
-            await repo.create({
-                id: entryId,
-                title: 'The title',
-                status: 'INITIAL',
-                createdBy: '123',
-                articleType: 'newspaper',
-            });
-            expect(mockKnex.insert).toBeCalledTimes(1);
-            expect(mockKnex.update).toBeCalledTimes(0);
-        });
         it('returns a complete DTO when passed a complete DTO to create', async (): Promise<void> => {
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
+            adapter.executor = jest.fn().mockReturnValue([]);
+            const repo = new XpubSubmissionRootRepository(adapter);
             const result = await repo.create({
                 id: entryId,
                 title: 'A Different Title',
@@ -200,15 +164,14 @@ describe('Knex Submission Repository', () => {
     });
     describe('delete', () => {
         it('returns true when knex delete is successful', () => {
-            mockKnex.delete = jest.fn().mockReturnValue(true);
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
+            adapter.executor = jest.fn().mockReturnValue(true);
+            const repo = new XpubSubmissionRootRepository(adapter);
             expect(repo.delete(SubmissionId.fromUuid(uuid()))).resolves.toEqual(true);
         });
         it('returns false when knex delete is not successful', () => {
-            mockKnex.delete = jest.fn().mockReturnValue(false);
-            const repo = new XpubSubmissionRootRepository((mockKnex as unknown) as Knex);
+            adapter.executor = jest.fn().mockReturnValue(false);
+            const repo = new XpubSubmissionRootRepository(adapter);
             expect(repo.delete(SubmissionId.fromUuid(uuid()))).resolves.toEqual(false);
         });
     });
-    */
 });
