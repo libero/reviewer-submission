8cc192abc4e626206128f790aa414d9094a39e30
diversemix <diversemix@gmail.com>
2020-02-21 15:37:19 +0000

    add wizard service

--- src/application/dashboard/resolvers.ts
+++ src/application/dashboard/resolvers.ts
@@ -3,16 +3,6 @@ import Submission from '../../domain/submission/services/models/submission';
 import { SubmissionId } from '../../domain/submission/types';
 import { IResolvers } from 'apollo-server-express';
 
-// TODO: move this out once work on this ticket start https://github.com/libero/reviewer-submission/issues/79
-export interface Person {
-    firstName: string;
-    lastName: string;
-    email: string;
-    institution: string;
-}
-
-export type Author = Person;
-
 const resolvers = (dashboard: DashboardService): IResolvers => ({
     Query: {
         async getSubmissions(): Promise<Submission[] | null> {
@@ -30,12 +20,6 @@ const resolvers = (dashboard: DashboardService): IResolvers => ({
         async deleteSubmission(_, { id }: { id: SubmissionId }): Promise<SubmissionId> {
             await dashboard.deleteSubmission(id);
             return id;
-        },
-
-        // stub pending - move to autosave service
-        async saveDetailsPage(_, { id, details }: { id: SubmissionId; details: Author }): Promise<Submission | null> {
-            // TODO: stub for now
-            return null;
         },
     },
 });
--- src/domain/submission/types.ts
+++ src/domain/submission/types.ts
@@ -1,3 +1,12 @@
 import { uuidType } from 'typesafe-uuid';
 
 export class SubmissionId extends uuidType<'SubmissionId'>() {}
+
+export interface Person {
+    firstName: string;
+    lastName: string;
+    email: string;
+    institution: string;
+}
+
+export type Author = Person;
