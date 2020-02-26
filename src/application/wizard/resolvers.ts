b8be8e1d19f0eb4433990f2e974b40d4f13423bf
Erez Odier <erez.odier@gmail.com>
2020-02-26 13:59:02 +0000

    WIP

--- src/application/wizard/resolvers.ts
+++ src/application/wizard/resolvers.ts
@@ -13,6 +13,45 @@ const resolvers = (wizard: WizardService): IResolvers => ({
         async uploadManuscript(_, variables: { file: FileUpload; id: SubmissionId }, context): Promise<null> {
             const { file, id: submissionId } = variables;
             const userId = context.userId;
+
+            // extract data
+            const stream = file.createReadStream();
+
+            const fileContents = await new Promise((resolve, reject) => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                const chunks: Array<any> = [];
+                stream.on('data', chunk => {
+                    chunks.push(chunk);
+                });
+                stream.on('error', reject);
+                stream.on('end', () => {
+                    resolve(Buffer.concat(chunks));
+                });
+            });
+
+            // validate
+
+            // get manuscript file
+            const manuscriptFile = fileRepo.getManuscriptBySubmissionId(submissionId);
+
+            if (manuscriptFile === null) {
+                // create file entry
+                const file = new File(submissionId, FileStatus.CREATED);
+
+                // save it
+                fileRepo.create(file);
+            }
+
+            // this can be parallel
+            // 2. upload to s3
+            const managedUpload = s3.upload(...);
+
+
+            // 3. send to scienceBeam
+            const scienceBeamPromise = scienceBeam.process(...);
+
+            Promise.all([managedUpload.promise(), scienceBeamPromise]);
+
             return null;
         },
         async uploadSupportingFile(_, variables: { file: FileUpload; id: SubmissionId }, context): Promise<null> {
