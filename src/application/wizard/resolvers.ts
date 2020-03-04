cab9cc81cbb0c5e05bc4d968205bd8a07b27368a
Erez Odier <erez.odier@gmail.com>
2020-03-04 13:11:11 +0000

    WIP

--- src/application/wizard/resolvers.ts
+++ src/application/wizard/resolvers.ts
@@ -2,12 +2,16 @@ import { WizardService } from './service';
 import Submission from '../../domain/submission/services/models/submission';
 import { SubmissionId, Author } from '../../domain/submission/types';
 import { IResolvers } from 'apollo-server-express';
+import { FileUpload } from 'graphql-upload';
 
 const resolvers = (wizard: WizardService): IResolvers => ({
     Query: {},
     Mutation: {
         async saveDetailsPage(_, { id, details }: { id: SubmissionId; details: Author }): Promise<Submission | null> {
             return wizard.saveDetailsPage(id, details);
+        },
+        async uploadSupportingFile(_, { id }: { id: SubmissionId, file: FileUpload }): Promise<Submission | null> {
+            return wizard.uploadSupportingFile(id, )
         },
     },
 });
