f29c0b462e106246fd1b484445e38947e3f399df
norisalsaadie <norisalsaadie@Noriss-MacBook-Pro.local>
2020-03-23 16:59:48 +0000

    Revert "WIP"
    This reverts commit 47fab576a9b2606e8e36dc252bea380e7d680dd8.

--- src/application/wizard/service.ts
+++ src/application/wizard/service.ts
@@ -126,7 +126,6 @@ export class WizardService {
         submissionId: SubmissionId,
         file: FileUpload,
         fileSize: number,
-        pubsub: PubSub,
     ): Promise<Submission> {
         const submission = await this.submissionService.get(submissionId);
         const allowed = this.permissionService.userCanWithSubmission(user, SubmissionOperation.UPDATE, submission);
@@ -143,8 +142,16 @@ export class WizardService {
             FileType.SUPPORTING_FILE,
         );
 
+        await new Promise((resolve, reject) => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            const chunks: Array<any> = [];
+            stream.on('data', chunk => chunks.push(chunk));
+            stream.on('error', reject);
+            stream.on('end', () => resolve(Buffer.concat(chunks)));
+        });
+
         try {
-            await this.fileService.uploadSupportingFile(stream, supportingFile, user.id, pubsub);
+            await this.fileService.uploadSupportingFile(supportingFile);
             supportingFile.setStatusToStored();
         } catch (e) {
             // @todo should this not be setStatusToDeleted ?
