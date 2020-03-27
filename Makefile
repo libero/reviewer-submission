58a24a284bf488c45a405800c1bf000fb9fdcf42
Noris Alsaadie <norisalsaadie@users.noreply.github.com>
2020-03-27 17:42:09 +0000

    Add config endpoint

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ LOAD_SCHEMA = psql -U postgres -f xpub-schema.sql
 setup:
 	if [ ! -e ./config/config.json ] ; then cp config/config.example.json config/config.json ; fi
 	if [ ! -e ./config/config.local.json ] ; then cp config/config.local.example.json config/config.local.json ; fi
+	if [ ! -e ./config/config.client.json ] ; then cp config/config.client.example.json config/config.client.json ; fi
 	git submodule update --init --recursive
 	$(MAKE) get_deps
 
@@ -38,7 +39,7 @@ setup_integration:
 test_integration: setup_integration
 	${DOCKER_COMPOSE_TEST} up -d application
 	./.scripts/docker/wait-healthy.sh test_reviewer-submission 20
-	CONFIG_PATH=./config/config.json yarn run test:integration
+	CONFIG_PATH=./config/config.json CLIENT_CONFIG_PATH=config/config.client.json yarn run test:integration
 	${DOCKER_COMPOSE_TEST} down
 
 load_schema:
---
+++ src/client-config.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from 'fs';
+
+export interface ClientPublicConfig {
+    client: {
+        majorSubjectAreas: { [key: string]: string };
+    };
+
+    login: {
+        url: string;
+        enableMock: boolean;
+        signupUrl: string;
+        legacySubmissionUrl: string;
+    };
+
+    fileUpload: {
+        maxSizeMB: number;
+    };
+
+    newrelic: {
+        licenseKey: string;
+        applicationId: string;
+    };
+
+    googleAnalytics: {
+        trackingId: string;
+    };
+
+    hotJar: {
+        enabled: boolean;
+        snippetVersion: number;
+    };
+
+    titles: { [key: string]: string };
+}
+
+const clientConfigPath = process.env.CLIENT_CONFIG_PATH
+    ? process.env.CLIENT_CONFIG_PATH
+    : '/etc/reviewer/config.client.json';
+
+const clientConfig: ClientPublicConfig = JSON.parse(readFileSync(clientConfigPath, 'utf8'));
+
+export default clientConfig;
