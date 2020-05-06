8d832f1bc893bfc0dac56153a285ceddbbef44a8
Erez Odier <erez.odier@gmail.com>
2020-05-06 16:22:08 +0100

    WIP

--- src/config.ts
+++ src/config.ts
@@ -34,7 +34,6 @@ export interface ClientPublicConfig {
     };
 }
 
-// const configPath = process.env.CONFIG_PATH ? process.env.CONFIG_PATH : '/etc/reviewer/config.json';
 const clientConfigPath = process.env.CLIENT_CONFIG_PATH
     ? process.env.CLIENT_CONFIG_PATH
     : '/etc/reviewer/config.client.json';
