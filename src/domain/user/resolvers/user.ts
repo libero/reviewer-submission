4c0a1584a2f2140efe8a8fa5520ddd76c86f76b1
diversemix <diversemix@gmail.com>
2020-05-20 11:38:19 +0100

    Add Authentication to the getEditors endpoint (#177)

--- src/domain/user/resolvers/user.ts
+++ src/domain/user/resolvers/user.ts
@@ -7,8 +7,8 @@ const resolvers = (userService: UserService): IResolvers => ({
         async getCurrentUser(_, args, context: { authorizationHeader: string }): Promise<User> {
             return await userService.getCurrentUser(context.authorizationHeader);
         },
-        async getEditors(_, args, role: string): Promise<EditorAlias[]> {
-            return await userService.getEditors(role);
+        async getEditors(_, args: { role: string }, context: { authorizationHeader: string }): Promise<EditorAlias[]> {
+            return await userService.getEditors(context.authorizationHeader, args.role);
         },
     },
 });
