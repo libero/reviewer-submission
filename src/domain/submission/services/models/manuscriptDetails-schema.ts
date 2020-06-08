78533eb87d868e8d7d09ba4ab0c651b4ce7f8474
diversemix <diversemix@gmail.com>
2020-06-08 16:43:24 +0100

    Refactor Joi validation, add tests and account for special cases for feature articles (#190)
    * Start refactor and add tests
    
    * More tests
    
    * Finish tests and add special cases for feature articles

---
+++ src/domain/submission/services/models/manuscriptDetails-schema.ts
@@ -0,0 +1,25 @@
+import * as Joi from 'joi';
+
+const subjectSchema = Joi.array()
+    .min(1)
+    .max(2)
+    .required();
+
+const featureSubjectSchema = Joi.array().max(2);
+
+const base = {
+    title: Joi.string().required(),
+    previouslyDiscussed: Joi.string().allow('', null),
+    previouslySubmitted: Joi.array().items(Joi.string()),
+    cosubmission: Joi.array().items(Joi.string()),
+};
+
+export const manuscriptDetailsSchema = Joi.object({
+    ...base,
+    subjects: subjectSchema,
+});
+
+export const featureManuscriptDetailsSchema = Joi.object({
+    ...base,
+    subjects: featureSubjectSchema,
+});
--- src/domain/submission/services/models/submission-schema.ts
+++ src/domain/submission/services/models/submission-schema.ts
@@ -1,127 +1,40 @@
 import * as Joi from 'joi';
+import { filesSchema } from './files-schema';
+import { manuscriptDetailsSchema, featureManuscriptDetailsSchema } from './manuscriptDetails-schema';
+import { editorDetailsSchema, featureEditorDetailsSchema } from './editorDetails-schema';
+import { authorSchema } from './authorDetails-schema';
+import { disclosureSchema } from './disclosure-schema';
 
-/*
-   todo: these should come from config - as they change per article type
-*/
-const MIN_SUGGESTED_SENIOR_EDITORS = 2;
-const MAX_SUGGESTED_SENIOR_EDITORS = 6;
-const MIN_SUGGESTED_REVIEWING_EDITORS = 2;
-const MAX_SUGGESTED_REVIEWING_EDITORS = 6;
-const MAX_SUGGESTED_REVIEWERS = 6;
-
-const fileSchema = Joi.object({
-    id: Joi.string().required(),
-    submissionId: Joi.string().required(),
-    created: Joi.date().required(),
-    updated: Joi.date().required(),
-    type: Joi.string().required(),
-    filename: Joi.string().required(),
-    url: Joi.string().required(),
-    mimeType: Joi.string().required(),
-    size: Joi.number().required(),
-    status: Joi.string().required(),
-});
-
-const suggestionSchema = Joi.object({
-    value: Joi.string().required(),
-    fieldName: Joi.string().required(),
-});
-
-// todo: change this schema to reflect the actual one in use
 export const submissionSchema = Joi.object({
     id: Joi.string().required(),
     created: Joi.date().required(),
     updated: Joi.date().required(),
-    status: Joi.string().required(),
+    status: Joi.string()
+        .required()
+        .equal('INITIAL'),
     createdBy: Joi.string().required(),
-    articleType: Joi.string().required(),
-    manuscriptDetails: {
-        title: Joi.string().required(),
-        subjects: Joi.alternatives().when('articleType', {
-            is: 'feature',
-            then: Joi.array().max(2),
-            otherwise: Joi.array()
-                .min(1)
-                .max(2)
-                .required(),
-        }),
-
-        previouslyDiscussed: Joi.string().allow('', null),
-        previouslySubmitted: Joi.array().items(Joi.string()),
-        cosubmission: Joi.array()
-            .items(Joi.string())
-            .required(),
-    },
-    files: {
-        coverLetter: Joi.string().required(),
-        manuscriptFile: fileSchema.required(),
-        supportingFiles: Joi.array().items(fileSchema),
-    },
-    editorDetails: {
-        suggestedSeniorEditors: Joi.array()
-            .items(Joi.string().required())
-            .min(MIN_SUGGESTED_SENIOR_EDITORS)
-            .max(MAX_SUGGESTED_SENIOR_EDITORS)
-            .required(),
-        opposedSeniorEditors: Joi.array()
-            .items(Joi.string())
-            .required(),
-        opposedSeniorEditorsReason: Joi.string().when('opposedSeniorEditors', {
-            is: Joi.array().min(1),
-            then: Joi.string().required(),
-            otherwise: Joi.string().allow(''),
-        }),
-        suggestedReviewingEditors: Joi.array()
-            .items(Joi.string().required())
-            .min(MIN_SUGGESTED_REVIEWING_EDITORS)
-            .max(MAX_SUGGESTED_REVIEWING_EDITORS)
-            .required(),
-        opposedReviewingEditors: Joi.array()
-            .items(Joi.string())
-            .required(),
-        opposedReviewingEditorsReason: Joi.string().when('opposedReviewingEditors', {
-            is: Joi.array().min(1),
-            then: Joi.string().required(),
-            otherwise: Joi.string().allow(''),
-        }),
-        suggestedReviewers: Joi.array()
-            .items(
-                Joi.object().keys({
-                    name: Joi.string().required(),
-                    email: Joi.string()
-                        .email()
-                        .required(),
-                }),
-            )
-            .max(MAX_SUGGESTED_REVIEWERS),
-        opposedReviewers: Joi.array().items(
-            Joi.object().keys({
-                name: Joi.string().required(),
-                email: Joi.string()
-                    .email()
-                    .required(),
-            }),
-        ),
-        opposedReviewersReason: Joi.string().when('opposedReviewers', {
-            is: Joi.array().min(1),
-            then: Joi.string().required(),
-            otherwise: Joi.string().allow(''),
-        }),
-    },
-    disclosure: {
-        submitterSignature: Joi.string().required(),
-        disclosureConsent: Joi.bool().required(),
-    },
-
-    suggestions: Joi.array().items(suggestionSchema),
-    author: Joi.object()
-        .keys({
-            firstName: Joi.string().required(),
-            lastName: Joi.string().required(),
-            email: Joi.string()
-                .email()
-                .required(),
-            aff: Joi.string().required(),
-        })
-        .required(),
+    articleType: Joi.string()
+        .required()
+        .valid([
+            'research-article',
+            'feature',
+            'research-advance',
+            'scientific-correspondence',
+            'tools-resources',
+            'short-report',
+        ]),
+    manuscriptDetails: Joi.when('articleType', {
+        is: 'feature',
+        then: featureManuscriptDetailsSchema,
+        otherwise: manuscriptDetailsSchema,
+    }),
+    files: filesSchema,
+    editorDetails: Joi.when('articleType', {
+        is: 'feature',
+        then: featureEditorDetailsSchema,
+        otherwise: editorDetailsSchema,
+    }),
+    disclosure: disclosureSchema,
+    suggestions: Joi.array(), // allow anything as not required for submission
+    author: authorSchema,
 });
