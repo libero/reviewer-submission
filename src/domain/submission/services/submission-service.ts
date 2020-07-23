8fc75bf126c85af80e0717979fbddcfc57e8238c
noris alsaadie <>
2020-07-23 14:58:41 +0100

    WIP

--- src/domain/submission/services/submission-service.ts
+++ src/domain/submission/services/submission-service.ts
@@ -81,12 +81,43 @@ export class SubmissionService {
 
         submission.status = SubmissionStatus.MECA_EXPORT_PENDING;
         await this.submissionRepository.update(submission);
+        const toEmail = submission.author?.email;
+        const data = {
+            html: `<p>Dear ${submission.author?.firstName}</p>,
+            <p>Thank you for submitting your work, "${submission.manuscriptDetails.title}", to eLife using our new submission interface. Your submission has now been transferred to our legacy system where the editorial evaluation will be carried out.</p>
+            <p>You will hear from us again shortly once your submission has undergone our quality check process, at which point you will receive a link to track the progress of your submission.</p>
+            
+            <p>Best wishes,<p>
+            <p>Nicola<p>
+            <p>Nicola Adamson (Editorial Assistant)</p>
+            <p>
+              eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address, Westbrook Centre, Milton Road, Cambridge CB4 1YG.
+            </p>
+            <p>
+              You are receiving this email because you have been identified as the corresponding author of a submission to eLife. If this isn't you please contact editorial@elifesciences.org
+            </p>
+            `,
+            text: `Dear ${submission.author?.firstName},
+            Thank you for submitting your work, "${submission.manuscriptDetails.title}", to eLife using our new submission interface. Your submission has now been transferred to our legacy system where the editorial evaluation will be carried out.
 
-        // @TODO: send submit email 
+            You will hear from us again shortly once your submission has undergone our quality check process, at which point you will receive a link to track the progress of your submission.
+
+            Best wishes,
+
+            Nicola
+
+            Nicola Adamson (Editorial Assistant)
+
+            eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address, Westbrook Centre, Milton Road, Cambridge CB4 1YG.
+
+            You are receiving this email because you have been identified as the corresponding author of a submission to eLife. If this isn't you please contact editorial@elifesciences.org.`,
+        };
+
+        // @TODO: send submit email
         // questions:
         // 1. which email address gets emailed
         // 2. BCC/CC required?
-        // this.mailService.sendEmail();
+        this.mailService.sendEmail();
 
         this.runMecaExport(submission, ip);
         return this.get(id);
