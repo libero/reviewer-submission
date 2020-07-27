664f1ae4c42a95d44b3da2d67a306ec1729be066
noris alsaadie <>
2020-07-27 09:39:50 +0100

    move submission email content to own file

--- src/domain/submission/services/submission-service.ts
+++ src/domain/submission/services/submission-service.ts
@@ -10,6 +10,7 @@ import { SftpStore } from './storage/sftp-store';
 import { SubmissionStore } from './storage/submission-store';
 import { InfraLogger as logger } from '../../../logger';
 import { MailService } from '../../mail/services/mail-service';
+import { submittedEmail } from './emails';
 
 export class SubmissionService {
     submissionRepository: XpubSubmissionRootRepository;
@@ -36,8 +37,6 @@ export class SubmissionService {
         const store = new SubmissionStore([this.s3Store, this.sftpStore]);
         const locations = await store.write(id, buffer);
         logger.info(`Submission ${id} saved to ${locations}`);
-
-        // @todo: email notification to author
     }
 
     async findAll(): Promise<Submission[]> {
@@ -82,35 +81,10 @@ export class SubmissionService {
         submission.status = SubmissionStatus.MECA_EXPORT_PENDING;
         await this.submissionRepository.update(submission);
         const toEmail = submission.author?.email;
+        const emailContent = submittedEmail(submission.author?.firstName, submission.manuscriptDetails.title);
         const data = {
-            html: `<p>Dear ${submission.author?.firstName}</p>,
-            <p>Thank you for submitting your work, "${submission.manuscriptDetails.title}", to eLife using our new submission interface. Your submission has now been transferred to our legacy system where the editorial evaluation will be carried out.</p>
-            <p>You will hear from us again shortly once your submission has undergone our quality check process, at which point you will receive a link to track the progress of your submission.</p>
-            
-            <p>Best wishes,<p>
-            <p>Nicola<p>
-            <p>Nicola Adamson (Editorial Assistant)</p>
-            <p>
-              eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address, Westbrook Centre, Milton Road, Cambridge CB4 1YG.
-            </p>
-            <p>
-              You are receiving this email because you have been identified as the corresponding author of a submission to eLife. If this isn't you please contact editorial@elifesciences.org
-            </p>
-            `,
-            text: `Dear ${submission.author?.firstName},
-            Thank you for submitting your work, "${submission.manuscriptDetails.title}", to eLife using our new submission interface. Your submission has now been transferred to our legacy system where the editorial evaluation will be carried out.
-
-            You will hear from us again shortly once your submission has undergone our quality check process, at which point you will receive a link to track the progress of your submission.
-
-            Best wishes,
-
-            Nicola
-
-            Nicola Adamson (Editorial Assistant)
-
-            eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address, Westbrook Centre, Milton Road, Cambridge CB4 1YG.
-
-            You are receiving this email because you have been identified as the corresponding author of a submission to eLife. If this isn't you please contact editorial@elifesciences.org.`,
+            html: emailContent.html,
+            text: emailContent.text,
         };
         await this.mailService.sendEmail(data.text, data.html, 'Your eLife submission', [toEmail || '']);
         this.runMecaExport(submission, ip);
