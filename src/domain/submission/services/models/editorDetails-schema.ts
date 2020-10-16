8d9b31fd921b6cd54a9d2893f6fd3e190bcf0f98
Cory <c.donkin@elifesciences.org>
2020-10-16 14:57:07 +0100

    replace joi .email with yups regx check

--- src/domain/submission/services/models/editorDetails-schema.ts
+++ src/domain/submission/services/models/editorDetails-schema.ts
@@ -1,5 +1,6 @@
 import * as Joi from 'joi';
-
+// We use same email regex as yup instead of joi .email() as the front end is more permissive causing server side errors on submit when email is a typo
+const EMAIL_REGEX = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
 const MIN_SUGGESTED_SENIOR_EDITORS = 2;
 const MAX_SUGGESTED_SENIOR_EDITORS = 6;
 const MIN_SUGGESTED_REVIEWING_EDITORS = 2;
@@ -49,7 +50,7 @@ const base = {
             Joi.object().keys({
                 name: Joi.string().required(),
                 email: Joi.string()
-                    .email()
+                    .regex(EMAIL_REGEX)
                     .required(),
             }),
         )
@@ -59,7 +60,7 @@ const base = {
             Joi.object().keys({
                 name: Joi.string().required(),
                 email: Joi.string()
-                    .email()
+                    .regex(EMAIL_REGEX)
                     .required(),
             }),
         )
