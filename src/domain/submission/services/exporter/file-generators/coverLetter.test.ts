2124fb9a5d39c662b24c4532cdbadb03ce6fc199
Cory <c.donkin@elifesciences.org>
2021-08-05 16:02:01 +0100

    add coverLetter test

--- src/domain/submission/services/exporter/file-generators/coverLetter.test.ts
+++ src/domain/submission/services/exporter/file-generators/coverLetter.test.ts
@@ -10,38 +10,40 @@ export interface ThingWithT {
 }
 
 const coverLetter = "<h1>Pick Me!</h1><p>I'm simply the best</p>";
+const getTextFromPDF = (document: Buffer): Promise<{ jsonData: string; errors: number }> =>
+    new Promise(resolve => {
+        const pdfParser = new PDFParser();
+        let errors = 0;
 
+        pdfParser.on('pdfParser_dataError', (err: string) => {
+            if (err) errors += 1;
+            resolve({ errors, jsonData: '' });
+        });
+        pdfParser.on('pdfParser_dataReady', () => {
+            const text = pdfParser.data.Pages[0].Texts.map((item: ThingWithR) =>
+                item.R.map((t: ThingWithT) => t.T).reduce((prev: string, curr: string) => prev + curr),
+            ).reduce((prev: string, curr: string) => prev + curr);
+
+            resolve({ jsonData: JSON.stringify(text, null, 2), errors });
+        });
+        pdfParser.parseBuffer(document);
+    });
 describe('Cover letter PDF generator', () => {
     it('returns PDF document', async () => {
         const pdf = await generateCoverLetter(coverLetter);
         const pdfString = pdf.toString();
         expect(pdfString.indexOf('%PDF')).toBe(0);
     });
+    it('returns empty PDF document if cover letter is empty', async () => {
+        const document = await generateCoverLetter();
+        const { jsonData, errors } = await getTextFromPDF(document);
+        expect(errors).toBe(0);
+        expect(jsonData).toBe('"%20C%20overLetter"');
+    });
     it('PDF document contains the cover letter', async () => {
         const document = await generateCoverLetter(coverLetter);
-
-        const pdfParser = new PDFParser();
-        let errors = 0;
-
-        pdfParser.on('pdfParser_dataError', (err: string) => {
-            if (err) errors += 1;
-        });
-        expect.assertions(3);
-        const donePromise = new Promise<boolean>(resolve => {
-            pdfParser.on('pdfParser_dataReady', () => {
-                expect(errors).toBe(0);
-                expect(pdfParser.data).not.toBe(null);
-
-                const text = pdfParser.data.Pages[0].Texts.map((item: ThingWithR) =>
-                    item.R.map((t: ThingWithT) => t.T).reduce((prev: string, curr: string) => prev + curr),
-                ).reduce((prev: string, curr: string) => prev + curr);
-
-                const jsonData = JSON.stringify(text, null, 2);
-                expect(jsonData).toBe('"%20C%20overLetterPickMe!I\'msimplythebest"');
-                resolve();
-            });
-        });
-        await pdfParser.parseBuffer(document);
-        await donePromise;
+        const { jsonData, errors } = await getTextFromPDF(document);
+        expect(errors).toBe(0);
+        expect(jsonData).toBe('"%20C%20overLetterPickMe!I\'msimplythebest"');
     });
 });
