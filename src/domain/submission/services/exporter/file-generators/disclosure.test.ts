d9af45735ca154828bcff81acea9c7eb713b6ce7
NuclearRedeye <j.summerfield@elifesciences.org>
2022-02-02 12:52:20 +0000

    refactor: support for match by text

--- src/domain/submission/services/exporter/file-generators/disclosure.test.ts
+++ src/domain/submission/services/exporter/file-generators/disclosure.test.ts
@@ -1,28 +1,59 @@
-import * as PDFParser from 'pdf2json';
 import { generateDisclosure } from './disclosure';
 import submission from './article.test.data';
 
-function toContainChunk(data: Buffer, chunk: Buffer): boolean {
-    let offset = 0;
-    do {
-        if (data[offset] === chunk[0]) {
-            for (let i = 1; i < chunk.length; i++) {
-                if (data[offset + i] !== chunk[i]) {
-                    break;
-                }
+function stringToHex(data: string): string {
+    let result = '';
+    for (let i = 0; i < data.length; i++) {
+        result += data.charCodeAt(i).toString(16);
+    }
+    console.log(`string: ${data}, hex: ${result}`);
+    return result;
+}
 
-                if (i + 1 === chunk.length) {
-                    return true;
-                }
-            }
+function pdfContainsText(pdf: Buffer, text: string): boolean {
+    const hexText = stringToHex(text);
+    let sectionStart = pdf.indexOf('[', 0);
+    while (sectionStart !== -1) {
+        const sectionEnd = pdf.indexOf(']', sectionStart);
+        if (sectionEnd === -1) {
+            return false;
         }
-    } while (++offset < data.length - chunk.length);
+
+        let segment = '';
+        let textChunkStart = pdf.indexOf('<', sectionStart);
+        while (textChunkStart > sectionStart && textChunkStart < sectionEnd) {
+            const textChunkEnd = pdf.indexOf('>', textChunkStart);
+
+            for (let i = textChunkStart + 1; i < textChunkEnd; i++) {
+                segment += String.fromCharCode(pdf[i]);
+            }
+
+            textChunkStart = pdf.indexOf('<', textChunkEnd);
+        }
+
+        if (segment === hexText) {
+            return true;
+        }
+
+        sectionStart = pdf.indexOf('[', sectionEnd);
+    }
     return false;
 }
 
 describe('Disclosure PDF generator', () => {
     it('returns a string of a PDF', async () => {
         const document = await generateDisclosure(submission, '1.2.3.4');
-        expect(toContainChunk(document, Buffer.from(`5 0 obj\n<<\n/Length 5680\n`))).toBe(true);
+        expect(pdfContainsText(document, `604e06ca-882d-4b5b-a147-e016893e60e9`)).toBe(true);
+        expect(pdfContainsText(document, `Test User`)).toBe(true);
+        expect(pdfContainsText(document, `A.Scientist`)).toBe(true);
+        expect(pdfContainsText(document, `1.2.3.4`)).toBe(true);
+        expect(pdfContainsText(document, `Disclosure of Data to Editors`)).toBe(true);
+        expect(
+            pdfContainsText(
+                document,
+                `Our privacy policy explains that we share your personal information with various third `,
+            ),
+        ).toBe(true);
+        expect(pdfContainsText(document, `I agree on behalf of all authors`)).toBe(true);
     });
 });
